9cc05f7b8b018d34b1e92fbd714b96819db1d6dc
inseybo <mikhail@shakacode.com>
2025-05-12 12:45:56 +0700

    vim: update global settings

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -1,13 +1,24 @@
+-- -- [[ Global ]] -- --
+
+vim.ortermguicolors = true
 vim.g.mapleader = " "
 vim.opt.clipboard:append("unnamedplus") -- copy to system clipboard
-vim.o.termguicolors = true
+vim.opt.mouse = "a" -- enable mouse support
+vim.opt.backupcopy = "yes" -- create backup copy of file on save
 
+-- -- [[ Folding ]] -- --
+vim.opt.foldenable = false -- don't fold on file open
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
+-- -- [[ Tabline ]] -- --
+vim.opt.showtabline = 2 -- always visible
 
+-- -- [[ History ]] -- --
+vim.opt.undofile = true
+vim.opt.undolevels = 1000
 
--- WHAT IS IT???
--- vim.g.maplocalleader = "\\"
-
-
-
-
+-- -- [[ Indentation ]] -- --
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
