57b3f80f6481321739a97af97b19df01883ef0b9
Mikhail Mirzoyan <mikhail@shakacode.com>
2025-06-26 19:26:28 +0700

    vim: add git-blame plug

---
+++ .config/nvim/lua/plugins/git-blame.lua
@@ -0,0 +1,15 @@
+return {
+  'f-person/git-blame.nvim',
+  event = 'VeryLazy',
+  opts = {
+    message_template = ' <summary> • <date> • <author> • <<sha>>',
+    date_format = '%Y-%m-%d %H:%M:%S',
+  },
+  config = function(_, opts)
+    require('gitblame').setup(opts)
+    vim.g.gitblame_message_when_not_committed = ' :: You better commit it'
+  end,
+  keys = {
+    { '<c-t>b', ':GitBlameToggle<cr>', desc = '[Toggle] Git blame', silent = true },
+  },
+}
