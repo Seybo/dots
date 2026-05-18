30326028169c4c9c186d3c9bde37b3bba5a9d8d4
Mikhail Mirzoyan <mikhail@shakacode.com>
2026-05-18 11:27:56 +0700

    env: add github light theme

---
+++ themes/github-light-default/fzf.zsh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --ansi
+	--color=fg:#656d76,bg:#ffffff,hl:#ffffff
+	--color=fg+:#1f2328,bg+:#deeeff,hl+:#953800
+	--color=border:#d0d7de,header:#eff1f3,gutter:#ffffff
+	--color=spinner:#24292f,info:#9a6700
+	--color=pointer:#8250df,marker:#1a7f37,prompt:#0969da"
