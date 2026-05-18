7f23e6fab24e398890387d5531b5f12530df8845
Mikhail Mirzoyan <mikhail@shakacode.com>
2026-05-18 08:50:26 +0700

    env: add nightfox dayfox theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,7 +1,16 @@
 return {
-  'rose-pine/neovim',
-  config = function()
-    -- the whole env theme is managed by the theme script: themes/theme_switcher.rb and its zsh alias 'theme'
-    dofile(vim.env.STOW_DIR .. '/themes/active/nvim.lua')
-  end,
+  {
+    'rose-pine/neovim',
+  },
+  {
+    'EdenEast/nightfox.nvim',
+  },
+  {
+    name = 'active-theme',
+    dir = vim.fn.stdpath('config'),
+    config = function()
+      -- the whole env theme is managed by the theme script: themes/theme_switcher.rb and its zsh alias 'theme'
+      dofile(vim.env.STOW_DIR .. '/themes/active/nvim.lua')
+    end,
+  },
 }
