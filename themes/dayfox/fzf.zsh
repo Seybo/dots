2629705174e9295641c0a4d4e9efc51485ef92e2
Mikhail Mirzoyan <mikhail@shakacode.com>
2026-05-18 08:50:26 +0700

    env: add nightfox dayfox theme

---
+++ themes/dayfox/fzf.zsh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --ansi
+	--color=fg:#534c45,bg:#f6f2ee,hl:#955f61
+	--color=fg+:#3d2b5a,bg+:#e7d2be,hl+:#955f61
+	--color=border:#d3c7bb,header:#396847,gutter:#f6f2ee
+	--color=spinner:#ac5402,info:#287980
+	--color=pointer:#6e33ce,marker:#a5222f,prompt:#534c45"
