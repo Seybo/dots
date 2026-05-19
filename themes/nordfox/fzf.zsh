77d590ea433d70b4268f27b24e45cbacd918f508
Mikhail Mirzoyan <mikhail@shakacode.com>
2026-05-19 11:51:16 +0700

    env: add dawnfox and nordfox themes

---
+++ themes/nordfox/fzf.zsh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --ansi
+	--color=fg:#abb1bb,bg:#2e3440,hl:#c9826b
+	--color=fg+:#cdcecf,bg+:#3e4a5b,hl+:#c9826b
+	--color=border:#444c5e,header:#a3be8c,gutter:#2e3440
+	--color=spinner:#ebcb8b,info:#88c0d0
+	--color=pointer:#b48ead,marker:#bf616a,prompt:#abb1bb"
