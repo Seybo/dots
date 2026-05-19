6bc5fca2f6b745250b50119f775a91e5d3fcdcc0
Mikhail Mirzoyan <mikhail@shakacode.com>
2026-05-19 12:44:23 +0700

    themes: add everforst hard themes

---
+++ themes/everforest-hard-dark/fzf.zsh
@@ -0,0 +1,9 @@
+# Everforest hard dark fzf colors.
+# Converted from canonical upstream Everforest palette:
+# https://github.com/sainnhe/everforest/blob/master/palette.md
+export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --ansi
+	--color=fg:#D3C6AA,bg:#272E33,hl:#E69875
+	--color=fg+:#D3C6AA,bg+:#2E383C,hl+:#E69875
+	--color=border:#495156,header:#7FBBB3,gutter:#272E33
+	--color=spinner:#DBBC7F,info:#83C092
+	--color=pointer:#D699B6,marker:#E67E80,prompt:#A7C080"
---
+++ themes/everforest-hard-dark/nvim.lua
@@ -0,0 +1,6 @@
+-- Official upstream Everforest Neovim theme:
+-- https://github.com/sainnhe/everforest
+vim.o.background = 'dark'
+vim.g.everforest_background = 'hard'
+vim.g.everforest_enable_italic = 1
+vim.cmd('colorscheme everforest')
