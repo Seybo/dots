5365e197e4d3d9c7b2db06b8069477391f3678d8
Mikhail Mirzoyan <mikhail@shakacode.com>
2026-05-26 19:12:50 +0700

    theme: update everforest-hard-light and add Pi to themes

--- themes/everforest-hard-light/nvim.lua
+++ themes/everforest-hard-light/nvim.lua
@@ -1,6 +1,11 @@
--- Official upstream Everforest Neovim theme:
--- https://github.com/sainnhe/everforest
+-- Everforest hard light Neovim colors.
+-- Customized local variant using the official upstream Everforest theme.
+-- Upstream: https://github.com/sainnhe/everforest
 vim.o.background = 'light'
 vim.g.everforest_background = 'hard'
 vim.g.everforest_enable_italic = 1
+vim.g.everforest_colors_override = {
+  fg = { '#3D484D', '239' },
+  bg0 = { '#FFFDF6', '230' },
+}
 vim.cmd('colorscheme everforest')
