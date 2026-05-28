0cbef6623b7cc134a981d72edd8902443d096fdd
Mikhail Mirzoyan <mikhail@shakacode.com>
2026-05-28 13:26:17 +0700

    theme: adjust everforest-hard-light colors a bit

--- themes/everforest-hard-light/fzf.zsh
+++ themes/everforest-hard-light/fzf.zsh
@@ -6,5 +6,5 @@ export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --ansi
 	--color=fg:#3D484D,bg:#FFFDF6,hl:#F57D26
 	--color=fg+:#3D484D,bg+:#F8F5E4,hl+:#F57D26
 	--color=border:#E8E5D5,header:#3A94C5,gutter:#FFFDF6
-	--color=spinner:#DFA000,info:#35A77C
-	--color=pointer:#DF69BA,marker:#F85552,prompt:#8DA101"
+	--color=spinner:#DFA000,info:#267B5F
+	--color=pointer:#DF69BA,marker:#F85552,prompt:#697801"
