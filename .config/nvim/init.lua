6cce8c53463ab1c31b821dfeefb14e3c5a9b169d
Mikhail Mirzoyan <mikhail@shakacode.com>
2026-08-28 08:08:43 +0700

    nvim: make shared configuration portable

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,10 @@
 require('config.debugger')
 require('config.settings')
+
+if vim.uv.os_uname().sysname == 'Linux' then
+  require('config.remote_clipboard').setup()
+end
+
 require('config.lazy')
 require('config.mappings')
 require('config.autocommands')
